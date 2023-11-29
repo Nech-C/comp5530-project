98d2571da67df885c0f761c0c6fd94c3d54a7f96
Chen <xiaokunchen@outlook.com>
2023-11-28 23:33:02 -0500

    feat: add functions for trainning and evaluation

--- utils.py
+++ utils.py
@@ -133,3 +133,164 @@ def load_reference_models(model_list, n):
         model.eval()
         reference_models.append(model)
     return reference_models
+
+
+def train_a2c(env, num_episodes, learning_rate, gamma, save_path=None):
+    """
+    Train an Actor-Critic model.
+    
+    Args:
+        env: The environment to train on.
+        num_episodes (int): The number of episodes to train for.
+        learning_rate (float): Learning rate for the optimizer.
+        gamma (float): Discount factor for rewards.
+        save_path (str, optional): Path to save the trained model.
+
+    Returns:
+        The trained ActorCritic model.
+    """
+    model = ActorCritic()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    for episode in range(num_episodes):
+        state = env.reset()
+        done = False
+        model.reset_trajectory()
+
+        while not done:
+            state_tensor = torch.FloatTensor(state).reshape(1, 3).float()
+            action_probs, value = model(state_tensor)
+            action_dist = torch.distributions.Categorical(action_probs)
+            action = action_dist.sample()
+            next_state, reward, done, _ = env.step(action.item())
+            log_prob = action_dist.log_prob(action)
+
+            model.log_probs.append(log_prob)
+            model.values.append(value)
+            model.rewards.append(reward)
+
+            state = next_state
+
+        # Update the model at the end of each episode
+        returns = get_discounted_rewards(model.rewards, gamma)
+        bprop_with_log_prob(model.log_probs, model.values, returns, optimizer)
+
+    if save_path:
+        torch.save(model.state_dict(), save_path)
+
+    return model
+
+def train_ppo(env, num_episodes, learning_rate, gamma, epsilon, beta, save_path=None):
+    """
+    Train a PPO model.
+    
+    Args:
+        env: The environment to train on.
+        num_episodes (int): The number of episodes to train for.
+        learning_rate (float): Learning rate for the optimizer.
+        gamma (float): Discount factor for rewards.
+        epsilon (float): Clipping parameter for PPO.
+        beta (float): Coefficient for the entropy bonus.
+        save_path (str, optional): Path to save the trained model.
+
+    Returns:
+        The trained ActorCritic model.
+    """
+    model = ActorCritic()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    for episode in range(num_episodes):
+        state = env.reset()
+        done = False
+        model.reset_trajectory()
+
+        while not done:
+            state_tensor = torch.FloatTensor(state).reshape(1, 3).float()
+            action_probs, value = model(state_tensor)
+            action_dist = torch.distributions.Categorical(action_probs)
+            action = action_dist.sample()
+            next_state, reward, done, _ = env.step(action.item())
+            log_prob = action_dist.log_prob(action)
+            entropy = action_dist.entropy()
+
+            model.log_probs.append(log_prob)
+            model.values.append(value)
+            model.rewards.append(reward)
+            model.entropies.append(entropy)
+
+            state = next_state
+
+        # Update the model at the end of each episode
+        returns = get_discounted_rewards(model.rewards, gamma)
+        bprop_with_ppo(model.log_probs, model.values, model.entropies, returns, epsilon, beta, optimizer)
+
+    if save_path:
+        torch.save(model.state_dict(), save_path)
+
+    return model
+
+def bprop_with_ppo(log_probs, values, entropies, returns, epsilon, beta, optimizer):
+    """
+    Perform backpropagation using the PPO loss function.
+    Args:
+        log_probs (list): Log probabilities of actions.
+        values (list): Value estimates from the critic.
+        entropies (list): Entropy values for each action.
+        returns (Tensor): Discounted returns.
+        epsilon (float): Clipping parameter for PPO.
+        beta (float): Coefficient for the entropy bonus.
+        optimizer (Optimizer): The optimizer to use.
+    """
+    advantages = returns - values.detach()
+    actor_loss = 0
+    critic_loss = 0
+    entropy_loss = 0
+
+    for log_prob, value, R, entropy in zip(log_probs, values, returns, entropies):
+        advantage = R - value.item()
+        ratio = torch.exp(log_prob - log_prob.detach())
+        surr1 = ratio
+        surr2 = torch.clamp(ratio, 1 - epsilon, 1 + epsilon) * advantage
+        actor_loss += -torch.min(surr1, surr2)
+        critic_loss += nn.functional.mse_loss(value.flatten(), torch.tensor([R], dtype=torch.float))
+        entropy_loss += -entropy
+
+    total_loss = actor_loss + 0.5 * critic_loss + beta * entropy_loss
+
+    optimizer.zero_grad()
+    total_loss.backward()
+    optimizer.step()
+
+def evaluate_model(model, env, num_runs):
+    """
+    Evaluate the given model in the environment.
+
+    Args:
+        model (nn.Module): The trained model to evaluate.
+        env: The environment to evaluate the model on.
+        num_runs (int): Number of runs to perform the evaluation.
+
+    Returns:
+        float: The average reward over the specified number of runs.
+    """
+    total_reward = 0.0
+
+    for _ in range(num_runs):
+        state = env.reset()
+        done = False
+        episode_reward = 0.0
+
+        while not done:
+            state_tensor = torch.FloatTensor(state).reshape(1, 3).float()
+            action_probs, _ = model(state_tensor)
+            action = torch.argmax(action_probs).item()  # Choose the action with highest probability
+            next_state, reward, done, _ = env.step(action)
+
+            episode_reward += reward
+            state = next_state
+
+        total_reward += episode_reward
+
+    average_reward = total_reward / num_runs
+    return average_reward
+
