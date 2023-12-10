eb743c88011180761ec99ac186945a115896d315
Chen <xiaokunchen@outlook.com>
2023-12-10 15:37:30 -0500

    feat: add absolute cp difference

--- trainer.py
+++ trainer.py
@@ -6,6 +6,8 @@ import utils
 import random
 import json
 import numpy
+
+from cpgpo_absDiff import single_CPGPO_absDiff
 from rl_env.maze import MazeEnv, ActorCriticMaze, ActorCriticMazeV2
 from datetime import datetime
 
@@ -45,7 +47,6 @@ def train_maz_a2c(num_agents, path_a2c, config=A2C_CONFIG):
         trained_model = utils.train_a2c(env, model, config['epoch'], config['learning_rate'], config['gamma'], device)
         model_save_path = os.path.join(path_a2c, f"a2c_maze_model_{i}.pth")
         torch.save(trained_model.state_dict(), model_save_path)
-
 
 def train_maz_a2c_v2(num_agents, path_a2c, config=A2C_CONFIG):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -124,21 +125,71 @@ def train_maze_cpgpo_v2(num_agents, config):
         model_save_path = os.path.join(path_cpgpo, f"cpgpo_maze_model_{i}.pth")
         torch.save(trained_model.state_dict(), model_save_path)
 
+def train_maze_cpgpo_v2_absDiff(num_agents, config):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # Check and create CPGPO path
+    current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+    path_cpgpo = os.path.join(config['path_cpgpo'], current_time)
+    config["algo"] = "abs cp diff"
+    os.makedirs(path_cpgpo, exist_ok=True)
 
+    # Save the configuration
+    config_path = os.path.join(path_cpgpo, "config.json")
+    with open(config_path, 'w') as f:
+        json.dump(config, f, indent=4)
 
-if __name__ == "__main__":
-    cpgpo_config_v2 = {
+    # Train CPGPO Agents
+    for i in range(num_agents):
+        # Refresh the list of all models from the reference directory before each training
+        all_models = []
+
+        for root, dirs, files in os.walk(config['reference_directory']):
+            for filename in files:
+                if filename.endswith(".pth"):
+                    model_path = os.path.join(root, filename)
+                    model = ActorCriticMazeV2().to(device)
+                    model.load_state_dict(torch.load(model_path, map_location=device))
+                    model.eval()
+                    all_models.append(model)
+        print(len(all_models))
+        env = MazeEnv()
+        model = ActorCriticMazeV2().to(device)
+        reference_model = random.choice(all_models).to(device)
+        trained_model = single_CPGPO_absDiff(env, model, reference_model, config, device)
+        model_save_path = os.path.join(path_cpgpo, f"cpgpo_maze_model_{i}.pth")
+        torch.save(trained_model.state_dict(), model_save_path)
+
+def test_train_maze_cpgpo_v2_absDiff():
+    config = {
         'learning_rate': 0.0007,
         'epoch': 8000,
         'gamma': 0.99,
         'starting_n': 1,
         'n_growth': 0.1,  # Assuming this represents the growth rate per epoch or condition
         'max_n': 1,
-        'epsilon': 0.8,
-        'epsilon_decay': 0.004,  # Assuming this represents the decay rate per epoch or condition
+        'epsilon': 0.25,
+        'epsilon_decay': 0.001,  # Assuming this represents the decay rate per epoch or condition
         'min_epsilon': 0.0,
         'reference_directory': "./trained_models/maze/v2",  # Directory containing reference models
-        'path_cpgpo': "./trained_models/maze/trained_n1_cpgpo_models_v1",
+        'path_cpgpo': "./trained_models/maze/absdiff/trained_n1_cpgpo_models_v1",
     }
-    #train_maz_a2c(num_agents=50, path_a2c="trained_models/maze/v1/trained_a2c_models")
-    train_maze_cpgpo_v2(1, cpgpo_config_v2)
+    train_maze_cpgpo_v2_absDiff(1, config)
+
+if __name__ == "__main__":
+    # cpgpo_config_v2 = {
+    #     'learning_rate': 0.0007,
+    #     'epoch': 8000,
+    #     'gamma': 0.99,
+    #     'starting_n': 1,
+    #     'n_growth': 0.1,  # Assuming this represents the growth rate per epoch or condition
+    #     'max_n': 1,
+    #     'epsilon': 0.8,
+    #     'epsilon_decay': 0.004,  # Assuming this represents the decay rate per epoch or condition
+    #     'min_epsilon': 0.0,
+    #     'reference_directory': "./trained_models/maze/v2",  # Directory containing reference models
+    #     'path_cpgpo': "./trained_models/maze/trained_n1_cpgpo_models_v1",
+    # }
+    # #train_maz_a2c(num_agents=50, path_a2c="trained_models/maze/v1/trained_a2c_models")
+    # train_maze_cpgpo_v2(1, cpgpo_config_v2)
+
+    test_train_maze_cpgpo_v2_absDiff()
